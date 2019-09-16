8eb711e46da24d735c2cef7f911b2641740ab23c
pranapramudita <pranapramudita@gmail.c.com>
2019-09-16 20:33:07 +0700

    case sensitive ternyata guys

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Impal_Project';
+$config['base_url'] = 'http://localhost/IMPAL_Project';
 
 /*
 |--------------------------------------------------------------------------
