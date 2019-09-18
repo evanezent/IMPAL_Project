ada01d404c9f936e2a2d9a59bf388f923769e961
lulaaflah <47202083+lulaaflah@users.noreply.github.com>
2019-09-18 20:14:29 +0700

    Update User.php

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -49,6 +49,44 @@ class User extends CI_Controller {
 		$this->Member_model->register($data);
 		redirect('User','refresh');
 	}
+	
+	public function login()
+	{
+		$this->load->view('template/header');
+		$this->load->view('user/register');
+		$this->load->view('template/footer');
+	}
+	
+	public function login_db()
+	{
+		$username = $this->input->post('username');
+		$password = $this->input->post('password');
+		$where = array(
+			'username' => $username,
+			'password' => md5($password)
+			);
+		$cek = $this->Member_model->cek_login("admin",$where)->num_rows();
+		if($cek > 0){
+ 
+			$data_session = array(
+				'nama' => $username,
+				'status' => "login"
+				);
+ 
+			$this->session->set_userdata($data_session);
+ 
+			redirect(base_url("admin"));
+ 
+		}else{
+			echo "Terjadi kesalahan input username/password";
+		}
+
+	}
+	public function logout()
+	{
+		$this->session->sess_destroy();
+		redirect(base_url('login'));
+	}
 
 }
 
