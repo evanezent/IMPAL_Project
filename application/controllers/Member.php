db379aeec77aaca37f87e73d90029e9b217df152
pranapramudita <pranapramudita@gmail.com>
2019-09-20 14:02:28 +0700

    gausah load lagi, udah di auto load kok tenang

--- application/controllers/Member.php
+++ application/controllers/Member.php
@@ -3,14 +3,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Member extends CI_Controller
 {
-
-	public function __construct()
-	{
-		parent::__construct();
-		//Load Dependencies
-		$this->load->model('Event_model');
-		$this->load->helper('string');
-	}
 	public function index()
 	{
 		$this->load->view('template/header_member');
