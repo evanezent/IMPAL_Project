7ff4dd65ff1196d5df4f6652022b3ddd2b2de26b
Pratama Yoga <pratamayo16@gmail.com>
2019-09-23 11:41:34 +0700

    update DATABASE

--- database/evanue.sql
+++ database/evanue.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 02, 2019 at 12:37 AM
+-- Generation Time: Sep 23, 2019 at 06:40 AM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.3
 
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `events` (
   `idEvent` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `username` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `namaEvent` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `tanggalEvent` date DEFAULT NULL,
   `poster` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -43,7 +43,13 @@ CREATE TABLE `events` (
 --
 
 INSERT INTO `events` (`idEvent`, `username`, `namaEvent`, `tanggalEvent`, `poster`, `Validasi`, `delete_at`) VALUES
-('BX188', 'esmeralda', 'konser Dangdut', '2015-08-12', 'ho.jpg', 1, '2015-08-22');
+('A6jJKQyCfn', 'esmeralda', 'adasdad', NULL, 'Good_Menu_Sistem5.PNG', NULL, NULL),
+('BX188', 'esmeralda', 'konser Dangdut', '2015-08-12', 'ho.jpg', 1, '2015-08-22'),
+('evt-', 'esmeralda', 'adasdad', NULL, '', NULL, NULL),
+('evt-000', 'esmeralda', 'p', NULL, '', NULL, NULL),
+('evt-cGSRA', 'esmeralda', 'hjkgf', '2019-09-14', 'Good_Menu_Sistem7.PNG', 0, '0000-00-00'),
+('evt-WTdox', 'esmeralda', 'hjkgf', NULL, 'Alif2.jpg', NULL, NULL),
+('iwEDx6OzpG', 'esmeralda', 'asdaf', NULL, '', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -52,7 +58,7 @@ INSERT INTO `events` (`idEvent`, `username`, `namaEvent`, `tanggalEvent`, `poste
 --
 
 CREATE TABLE `member` (
-  `username` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `namaMember` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `email` varchar(30) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `password` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -75,7 +81,7 @@ INSERT INTO `member` (`username`, `namaMember`, `email`, `password`, `alamat`, `
 
 CREATE TABLE `payment` (
   `idTransaksi` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `username` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `username` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `jenisTransaksi` varchar(25) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `tanggalTransaksi` date DEFAULT NULL,
   `delete_at` date DEFAULT NULL
@@ -96,7 +102,7 @@ INSERT INTO `payment` (`idTransaksi`, `username`, `jenisTransaksi`, `tanggalTran
 
 CREATE TABLE `ticket` (
   `idTicket` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `username` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `namaTicket` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `tanggalTicket` date DEFAULT NULL,
   `poster` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -110,7 +116,9 @@ CREATE TABLE `ticket` (
 --
 
 INSERT INTO `ticket` (`idTicket`, `username`, `namaTicket`, `tanggalTicket`, `poster`, `contactPerson`, `Validasi`, `delete_at`) VALUES
-('1301188', 'esmeralda', 'konser Sabyan', '2019-03-13', 'o.jpg', '089324678234', 1, '2020-03-03');
+('1301188', 'esmeralda', 'konser Sabyan', '2019-03-13', 'o.jpg', '089324678234', 1, '2020-03-03'),
+('tkt-0Inab', 'esmeralda', 'adadad', '2019-09-25', 'Bad_Menu_Sistem.PNG', '08932424', 0, '0000-00-00'),
+('tkt-Ulp8j', 'esmeralda', 'ahay', '2019-09-21', 'Alif.jpg', NULL, 0, '0000-00-00');
 
 --
 -- Indexes for dumped tables
