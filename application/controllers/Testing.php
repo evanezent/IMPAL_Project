ec3c951a4c56c141612ac4cd60072453e9dba192
Prana Pramudita Kusdiananggalih <pranapramudita@gmail.com>
2019-12-18 20:20:56 +0700

    Unit Testing Input Payment

--- application/controllers/Testing.php
+++ application/controllers/Testing.php
@@ -66,6 +66,66 @@ class Testing extends CI_Controller {
         echo $this->unit->run($test,$expected_result,$test_name);
     }
 
+    public function confirm_payment()
+	{
+        $id_event = 'evt-owPu3';
+        // $gambar = $_FILES['foto_bayar']['name'];
+		$gambar = '';
+		$user =  'pranapramudita';
+		if ($gambar) {
+			$config['upload_path']          =  './upload/payment';
+			$config['allowed_types']        =  'gif|jpeg|jpg|png';
+			$config['max_size']             =  1000000000000;
+			$config['max_width']            =  5000;
+			$config['max_height']           =  5000;
+
+			$this->load->library('upload', $config);
+
+			if (!$this->upload->do_upload('upload/payment/fortnite-season-x-image-4k-3840x2160_477799-mm-90.jpg')) {
+				// $error = array('error' => $this->upload->display_errors());
+				// echo $error;
+				// $this->session->set_flashdata('fail_evt', 'Fail event');
+                // redirect('Member', 'refresh');
+                $test = False;
+			} else {
+				$foto = $this->upload->data();
+				$namafile = $foto['file_name'];
+				// Random id
+				$id = random_string('alnum', 5);
+
+				$data = array(
+					'idTransaksi' => $id,
+					'idEvent' => $id_event,
+					'username' => $user,
+					'tanggalTransaksi' => date("Y-m-d"),
+					'foto_bayar' => $namafile,
+					'delete_at' => null
+				);
+				$this->Payment_model->inputPayment($data);
+				$this->session->set_flashdata('sukses_evt', 'Success event');
+                // redirect('Member', 'refresh');
+                $test = True;
+            }
+		} else {
+			// Flash message foto kosong
+			// echo "GAMBAR KOSONG";
+			$this->session->set_flashdata('fail_pic', 'Fail pic');
+			// redirect('Member/payment/.echo $id_event', 'refresh');
+            // echo $error;
+            $test = False;
+        }
+        return $test;
+	}
+
+    public function testInputPayment(){
+        echo 'Unit Testing Login';
+        $test = $this->confirm_payment();
+        // $expected_result = True;
+        $expected_result = False;
+        $test_name = 'Upload Gambar Kosong';
+        echo $this->unit->run($test,$expected_result,$test_name);
+    }
+
     public function index()
     {
 
