1998d25b688595b1b6151bc4c8d69ecedaacc11c
Prana Pramudita Kusdiananggalih <pranapramudita@gmail.com>
2019-12-18 19:49:56 +0700

    Unit Testing Login

--- application/controllers/Testing.php
+++ application/controllers/Testing.php
@@ -10,15 +10,59 @@ class Testing extends CI_Controller {
         $this->load->library('unit_test');
     }
     
-    public function login(){
-        
-    }
+    public function login_db() 
+	{
+        // // username dan password benar
+		// $username = 'pranapramudita';
+        // $password = '123456';
 
+        // // username atau password salah
+		// $username = 'pranapramudita';
+        // $password = '12345';
+
+        // username atau password kosong
+		$username = '';
+        $password = '12345';
+
+		$where = array(
+			'username' => $username,
+			'password' => md5($password)
+		);
+		$cek = $this->Member_model->cek_login("member", $where)->num_rows();
+		if ($cek > 0) {
+
+			$data_session = array(
+				'nama' => $username,
+				'status' => "login"
+			);
+
+			$this->session->set_userdata('user', $data_session);
+
+            // redirect(base_url("member"));
+            $test = True;
+		} else {
+			$event = $this->Event_model->getEvent();
+			$ticket = $this->Ticket_model->getTicket();
+
+			$data['events'] = $this->Event_model->getEvent();
+			$data['salah'] = 2;
+			// $this->load->view('template/header', $data);
+			// $this->load->view('user/homepage_event');
+            // $this->load->view('template/footer');
+            $test = False;
+        }
+        if(is_null($username)){
+            $test = False;
+        }
+        return $test;
+    }
+    
     public function testLogin(){
         echo 'Unit Testing Login';
-        $test = $this->login();
-        $expected_result = True;
-        $test_name = 'login dengan angka';
+        $test = $this->login_db();
+        // $expected_result = True;
+        $expected_result = False;
+        $test_name = 'Username atau Password Kosong';
         echo $this->unit->run($test,$expected_result,$test_name);
     }
 
